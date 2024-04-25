4672a1c1508038f796cfb2c25ee638dacf738bcf
John Mark Tizado <johnmarktizado@gmail.com>
2024-04-26 03:32:54 +0800

    fix: remove unnecessary uploadFile function and fix onsave bugs where name, petType, and petActivityLevel saves as blank string or null

--- src/pages/profile/PetProfile.jsx
+++ src/pages/profile/PetProfile.jsx
@@ -6,40 +6,35 @@ import {
   addDoc,
   deleteDoc,
   doc,
-  updateDoc,
 } from "firebase/firestore";
 import {
   ref,
   uploadBytes,
   getDownloadURL,
-  uploadBytesResumable,
+  // uploadBytesResumable,
 } from "firebase/storage";
-import WeightComponent from "./getWeight";
 import FeedAmountComponent from "./feedAmountComponent";
+import GetWeight from "./getWeight";
 
 export default function PetProfile() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [petList, setPetList] = useState([]);
-  const [smartFeedingActivated, setSmartFeedingActivated] = useState(false); // State for smart feeding activation
+  const [smartFeedingActivated, setSmartFeedingActivated] = useState(false);
 
-  // New Pet States
   const [newPetName, setNewPetName] = useState("");
-  const [newPetType, setNewPetType] = useState("");
-  // const [petWeight, setNewPetWeight] = useState(0);
+  const [newPetType, setNewPetType] = useState("Cat");
   const [petWeight, setPetWeight] = useState(null);
-  const [newPetActivityLevel, setNewPetActivityLevel] = useState(0);
+  const [newPetActivityLevel, setNewPetActivityLevel] = useState(
+    newPetType === "Cat" ? "Intact" : "Intact"
+  ); // Set default value based on pet type;
 
-  // Update Activity Level State
-  const [updatedPetActivityLevel, setUpdatedPetActivityLevel] = useState(0);
-
-  // File Upload State
   const [file, setFile] = useState(null);
-  // const [imageList, setImageList] = useState([]);
   const [data, setData] = useState({});
-  const [per, setPerc] = useState(null);
+  // const [perc, setPerc] = useState(0);
 
   const petCollectionRef = useMemo(() => collection(db, "pets"), []);
 
+  // GET PET LIST FUNCTION (USE FOR RENDERING PETS)
   const getPetList = async () => {
     try {
       const data = await getDocs(petCollectionRef);
@@ -57,6 +52,7 @@ export default function PetProfile() {
     getPetList();
   }, []);
 
+  // SMART FEEDING STATE TOGGLE WITH VALUE SAVED IN LOCAL STORAGE. TRUE/FALSE
   useEffect(() => {
     // Retrieve smart feeding activation state from localStorage
     const savedSmartFeedingState = localStorage.getItem(
@@ -67,218 +63,255 @@ export default function PetProfile() {
     }
   }, []);
 
-  const toggleSmartFeeding = () => {
-    // Toggle smart feeding state
-    const newSmartFeedingState = !smartFeedingActivated;
-    setSmartFeedingActivated(newSmartFeedingState);
-    // Store updated smart feeding state in localStorage
-    localStorage.setItem(
-      "smartFeedingActivated",
-      JSON.stringify(newSmartFeedingState)
-    );
-  };
-
-  // Function to toggle the modal state
+  // TOGGLE MODAL STATE FUNCTION
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
+  // UPLOAD FILE FUNCTION
   useEffect(() => {
-    const uploadFile = () => {
-      const name = new Date().getTime() + file.name;
+    const uploadFile = async () => {
+      if (!file) return;
+      // const name = new Date().getTime() + file.name;
+      const name = file.name;
+      const filesFolderRef = ref(storage, `petImages/${name}`);
 
-      console.log(name);
-      const storageRef = ref(storage, file.name);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          setPerc(progress);
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-            default:
-              break;
-          }
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setData((prev) => ({ ...prev, img: downloadURL }));
-          });
-        }
-      );
+      try {
+        await uploadBytes(filesFolderRef, file);
+        const downloadURL = await getDownloadURL(filesFolderRef);
+        setData((prev) => ({ ...prev, img: downloadURL }));
+      } catch (error) {
+        console.error("Error uploading file:", error);
+      }
     };
-    file && uploadFile();
+
+    uploadFile();
   }, [file]);
 
-  // Function to handle saving data to Firestore
+  // SAVE PET FUNCTION
   const onSavePet = async () => {
-    // Add your Firestore logic here
     try {
-      await addDoc(petCollectionRef, {
+      const docData = {
         name: newPetName,
         petType: newPetType,
         weight: petWeight,
         activityLevel: newPetActivityLevel,
         userId: auth?.currentUser?.uid,
-        imageURL: data,
-      });
+      };
+
+      if (data.img) {
+        docData.imageURL = data.img;
+      }
+
+      const docRef = await addDoc(petCollectionRef, docData);
 
       getPetList();
-      console.log("Data saved to Firestore");
+      console.log("Data saved to Firestore with ID: ", docRef.id);
     } catch (err) {
       console.error(err);
     }
+    setNewPetName("");
+    setNewPetType("Cat");
+    setFile(null);
     setIsModalOpen(!isModalOpen);
   };
 
+  // DELETE PET FUNCTION
   const deletePet = async (id) => {
+    alert("Are you sure you want to delete this pet?");
     const petDocument = doc(db, "pets", id);
     await deleteDoc(petDocument);
   };
-  const updatePetActivityLevel = async (id) => {
-    const petDocument = doc(db, "pets", id);
-    await updateDoc(petDocument, { activityLevel: updatedPetActivityLevel });
-  };
-
-  // const imageListRef = ref(storage, "petImages/");
-  const uploadFile = async () => {
-    if (!file) return;
-    const filesFolderRef = ref(storage, `petImages/${file.name}`);
-    try {
-      await uploadBytes(filesFolderRef, file);
-      alert("image uploaded");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // useEffect(() => {
-  //   const uploadFile = () => {
-  //     const name = new Date().getTime() + file.name;
-  //     const storageRef = ref(storage, fileUpload.name);
-  //   };
-  //   file && uploadFile();
-  // }, [fileUpload]);
-
-  // useEffect(() => {
-  //   listAll(imageListRef).then((res) => {
-  //     res.items.forEach((item) => {
-  //       getDownloadURL(item).then((url) => {
-  //         setImageList((prev) => [...prev, url]);
-  //       });
-  //     });
-  //   });
-  // }, []);
 
   return (
     <div>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-3"
         onClick={toggleModal}
       >
         + Add Pet
       </button>
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {petList.map((pet) => (
-          <div key={pet.id}>
-            <img src={pet.imageURL} />
-            <h1>Name : {pet.name}</h1>
-            <p>Pet Type : {pet.petType}</p>
-            <p>Weight (KG) : {pet.weight}</p>
+          <div
+            key={pet.id}
+            className="relative flex flex-col border border-gray-300 rounded-md p-4 mb-2"
+          >
             <div className="flex">
-              <p>Pet Activity Level : {pet.activityLevel} </p>
-              <input
-                placeholder="New Pet's Activity Level"
-                onChange={(e) =>
-                  setUpdatedPetActivityLevel(Number(e.target.value))
-                }
-                className="ml-2 mr-2"
-              />
-              <button onClick={() => updatePetActivityLevel(pet.id)}>
-                Update Activity Level
+              <div>
+                <div className="w-40 h-40 rounded-xl overflow-hidden justify-center ml-4 mb-4">
+                  <img
+                    src={
+                      pet.imageURL ||
+                      "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                    }
+                    alt="Pet"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="ml-4">
+                  <h1 className="font-bold">{pet.name}</h1>
+                  <p className="text-gray-600">Pet Type: {pet.petType}</p>
+                  <p className="text-gray-600">Weight (KG): {pet.weight}</p>
+                  <p className="text-gray-600">
+                    Pet Activity Level: {pet.activityLevel}
+                  </p>
+                </div>
+              </div>
+
+              <div className="w-full flex justify-end mr-20">
+                {/* SMART FEDING ACTIVATED  */}
+                <FeedAmountComponent
+                  petId={String(pet.id)}
+                  petName={String(pet.name)}
+                  petType={pet.petType}
+                  weight={Number(pet.weight)}
+                  activityLevel={Number(pet.activityLevel)}
+                  smartFeedingActivated={Boolean(smartFeedingActivated)}
+                />
+              </div>
+            </div>
+
+            <div className="absolute top-0 right-0 m-2">
+              <button
+                onClick={() => deletePet(pet.id)}
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded"
+              >
+                &times;
               </button>
             </div>
-            <button onClick={() => deletePet(pet.id)}>Delete Pet</button>
-
-            {/* SMART FEEDING */}
-            <FeedAmountComponent
-              petId={String(pet.id)}
-              petName={String(pet.name)}
-              petType={pet.petType} // Use pet.petType instead of newPetType
-              weight={Number(pet.weight)} // Use pet.weight instead of petWeight
-              activityLevel={Number(pet.activityLevel)} // Use pet.activityLevel
-              smartFeedingActivated={Boolean(smartFeedingActivated)}
-            />
           </div>
         ))}
       </div>
 
+      {/* FOR ADD PET MODAL  */}
       {isModalOpen && (
         <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-8 rounded-md">
-            <button
-              className="absolute top-0 right-0 m-4 p-4 text-white hover:text-gray-800 border"
-              onClick={toggleModal}
-            >
-              &times;
-            </button>
-            <h1>CREATE PROFILE</h1>
+          <div className="bg-white p-8 rounded-md flex flex-col max-h-full overflow-auto">
+            <h1 className="text-xl font-semibold mb-4">CREATE PROFILE</h1>
             <hr />
-            <div className="flex justify-center m-10">
-              <img
-                src={
-                  file
-                    ? URL.createObjectURL(file)
-                    : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-                }
-                alt=""
-                className="h-48 flex"
-              />
+            <div className="flex justify-center items-center my-6">
+              <label
+                htmlFor="fileInput"
+                className="relative w-48 h-48 overflow-hidden rounded-full cursor-pointer"
+              >
+                <img
+                  src={
+                    file
+                      ? URL.createObjectURL(file)
+                      : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                  }
+                  alt=""
+                  className="w-full h-full object-cover"
+                />
+                <input
+                  id="fileInput"
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  className="absolute inset-0 opacity-0 cursor-pointer"
+                />
+              </label>
             </div>
-            <div className="flex items-center">
-              <div className="m-12 p-5"></div>
-              <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-              <button onClick={uploadFile}>Upload Image</button>
-            </div>
-            <div>
+            <div className="flex flex-col mb-4">
+              <label
+                htmlFor="petName"
+                className="text-sm font-medium text-gray-700 mb-1"
+              >
+                Pet Name:
+              </label>
               <input
-                placeholder="Name"
+                id="petName"
+                type="text"
+                placeholder="Enter pet's name"
+                value={newPetName}
                 onChange={(e) => setNewPetName(e.target.value)}
-                className="pl-2"
-              />
-              <input
-                placeholder="Pet Type"
-                onChange={(e) => setNewPetType(e.target.value)}
-                className="pl-2"
-              />
-              {/* GET PET WEIGHT FROM FIRESTORE */}
-              <WeightComponent setPetWeight={setPetWeight} />
-
-              <input
-                placeholder="Pet's Activity Level"
-                type="number"
-                onChange={(e) => setNewPetActivityLevel(Number(e.target.value))}
-                className="pl-2"
+                className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               />
             </div>
-            <button
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              onClick={onSavePet}
-            >
-              Save
-            </button>
+            <div className="flex flex-col mb-4">
+              <label
+                htmlFor="petType"
+                className="text-sm font-medium text-gray-700 mb-1"
+              >
+                Pet Type:
+              </label>
+              <select
+                id="petType"
+                className="p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={newPetType}
+                onChange={(e) => setNewPetType(e.target.value)}
+              >
+                <option value="Cat">Cat</option>
+                <option value="Dog">Dog</option>
+              </select>
+            </div>
+            <div className="flex flex-col mb-4">
+              <label
+                htmlFor="activityLevel"
+                className="text-sm font-medium text-gray-700 mb-1"
+              >
+                Activity Level:
+              </label>
+              <select
+                id="activityLevel"
+                className="p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={newPetActivityLevel}
+                onChange={(e) => setNewPetActivityLevel(e.target.value)}
+              >
+                {newPetType === "Cat" ? (
+                  <>
+                    <option value="Intact">Intact (Multiply RER by 1.4)</option>
+                    <option value="Neutered">
+                      Neutered (Multiply RER by 1.2)
+                    </option>
+                    <option value="Obesity Prone">
+                      Obesity Prone (Multiply RER by 1)
+                    </option>
+                    <option value="Weight Loss">
+                      Weight Loss (Multiply RER by 0.8)
+                    </option>
+                  </>
+                ) : (
+                  <>
+                    <option value="Intact">Intact (Multiply RER by 1.8)</option>
+                    <option value="Neutered">
+                      Neutered (Multiply RER by 1.6)
+                    </option>
+                    <option value="Obesity Prone">
+                      Obesity Prone (Multiply RER by 1.4)
+                    </option>
+                    <option value="Weight Loss">
+                      Weight Loss (Multiply RER by 1)
+                    </option>
+                    <option value="Light Work">
+                      Light Work (Multiply RER by 2)
+                    </option>
+                    <option value="Moderate Work">
+                      Moderate Work (Multiply RER by 3)
+                    </option>
+                    <option value="Heavy Work">
+                      Heavy Work (Multiply RER by 4 to 8)
+                    </option>
+                  </>
+                )}
+              </select>
+            </div>
+            {/* GET PET WEIGHT FROM FIRESTORE */}
+            <GetWeight setPetWeight={setPetWeight} />
+            <div className="flex mt-5 justify-around">
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                onClick={onSavePet}
+              >
+                Save
+              </button>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                onClick={toggleModal}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       )}
