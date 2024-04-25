f5814adf5601f211b51987868f401dfa9eda22f0
John Mark Tizado <johnmarktizado@gmail.com>
2024-04-26 03:24:08 +0800

    add: use of rtd for as trigger

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+// import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,4 +25,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+// export const rtd = getDatabase(app);
 // const analytics = getAnalytics(app);
