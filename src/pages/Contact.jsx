4d3a9c55adf7205b81b208985249518f83f9a4fa
John Mark Tizado <johnmarktizado@gmail.com>
2024-05-27 01:11:58 +0800

    add: FAQ to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
       <div className="flex justify-center">
         <img src="./src/assets/faq-logo.png" className="h-1/2 w-1/3" />
         <div className="flex flex-col">
-          <h1 className="text-5xl">Frequently Asked Questions</h1>
+          <h1 className="text-5xl">Frequently Asked Questions (FAQ)</h1>
         </div>
       </div>
     </div>
