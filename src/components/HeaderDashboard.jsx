96e817a1ead453e2c57c098a1305380691c84eea
John Mark Tizado <johnmarktizado@gmail.com>
2024-05-29 20:48:14 +0800

    fix: clickable logout as a whole header in the header

--- src/components/HeaderDashboard.jsx
+++ src/components/HeaderDashboard.jsx
@@ -4,11 +4,8 @@ import { CiLogout } from "react-icons/ci";
 export default function HeaderDashboard() {
   return (
     <>
-      <div className="Header flex bg-violetBG h-16 w-full backdrop-blur-sm shadow-md border-b-4 border-darkViolet">
-        <NavLink
-          to="/"
-          className={"flex flex-row-reverse content-center size-full"}
-        >
+      <div className="Header flex bg-violetBG h-16 w-full backdrop-blur-sm shadow-md border-b-4 border-darkViolet flex-row-reverse">
+        <NavLink to="/" className="flex flex-row-reverse content-center">
           <CiLogout className="self-center mr-10 cursor-pointer stroke-2 size-5" />
           <div className="font-medium text-lg self-center p-2"> Logout</div>
         </NavLink>
