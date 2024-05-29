1daeeaddc56c18a34027f05a98cf8f52e1ed28f1
John Mark Tizado <johnmarktizado@gmail.com>
2024-05-29 20:47:20 +0800

    fix: alignment of text and img in footer

--- src/components/FooterDashboard.jsx
+++ src/components/FooterDashboard.jsx
@@ -3,17 +3,19 @@ export default function FooterDashboard() {
     <>
       <footer className="footer bg-gray-700	 flex text-center">
         <div className="border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-3">
-          <a
-            href="#"
-            className="flex items-center justify-center m-3 text-xl font-semibold text-whiteViolet"
-          >
-            Anytime PETness
-          </a>
-          <img
-            src="../src/assets/petness-logo-icon.png"
-            className="h-6 mr-3 sm:h-9"
-            alt="PETness"
-          />
+          <div className="flex flex-row-reverse justify-center items-center">
+            <a
+              href="#"
+              className="flex items-center justify-center m-3 text-xl font-semibold text-whiteViolet"
+            >
+              Anytime PETness
+            </a>
+            <img
+              src="../src/assets/petness-logo-icon.png"
+              className="h-6 mr-3 sm:h-9"
+              alt="PETness"
+            />
+          </div>
           <div className="text-sm text-center text-white m-2">
             © 2023-2324 PETNESS™. All Rights Reserved.
           </div>
