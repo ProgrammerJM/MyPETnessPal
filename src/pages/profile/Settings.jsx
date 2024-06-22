94dcad6f4048e6fae7596f80032407b55dee10aa
John Mark Tizado <johnmarktizado@gmail.com>
2024-06-22 23:21:33 +0800

    preparation for dark mode

--- src/pages/profile/Settings.jsx
+++ src/pages/profile/Settings.jsx
@@ -17,14 +17,21 @@ export default function Settings() {
   const { toggleTheme } = useContext(ThemeContext);
   return (
     <>
-      <button
-        onClick={logout}
-        className="rounded-md border-5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
-      >
-        {" "}
-        Logout{" "}
-      </button>
-      <button onClick={toggleTheme}>Toggle Theme</button>
+      <div className="flex justify-between">
+        <button
+          onClick={logout}
+          className="rounded-md border-5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+        >
+          {" "}
+          Logout{" "}
+        </button>
+        <button
+          onClick={toggleTheme}
+          className="rounded-md border-5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+        >
+          Dark Mode (Soon)
+        </button>
+      </div>
     </>
   );
 }
