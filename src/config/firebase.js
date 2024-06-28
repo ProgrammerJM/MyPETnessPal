f0d4f04082e1b7b387242470f4b8343340fb2962
John Mark Tizado <johnmarktizado@gmail.com>
2024-06-29 01:49:23 +0800

    new: use env for api key protection

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,27 +5,30 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
+import { getMessaging } from "firebase/messaging"; // Add the Firebase products that you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPcMRU9x421wP0cS1sRHwEvi57W8NoLiE",
-  authDomain: "petness-92c55.firebaseapp.com",
-  projectId: "petness-92c55",
-  databaseURL:
-    "https://petness-92c55-default-rtdb.asia-southeast1.firebasedatabase.app",
-  storageBucket: "petness-92c55.appspot.com",
-  messagingSenderId: "115182665138",
-  appId: "1:115182665138:web:8782a3e937e136897e38ab",
-  measurementId: "G-Y0PW9RDDCN",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const realtimeDatabase = getDatabase(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const realtimeDatabase = getDatabase(app);
+const messaging = getMessaging(app);
+
+export { app, auth, db, storage, realtimeDatabase, messaging };
 // const analytics = getAnalytics(app);
