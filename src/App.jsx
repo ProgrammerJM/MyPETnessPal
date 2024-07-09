5ac87b6c0ee021043d86c36413c2d7fe572a8280
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-10 05:03:39 +0800

    add: notification provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PetProvider } from "./pages/function/PetContext";
+import { NotificationProvider } from "./pages/function/NotificationsContext";
 import { ThemeContext } from "./pages/function/ThemeContext";
 import { useContext } from "react";
 import Layout from "./components/Layout";
@@ -25,34 +26,40 @@ function App() {
   const { theme } = useContext(ThemeContext); // use ThemeContext
 
   return (
-      <BrowserRouter>
-        <div className={theme === "light" ? "light-class" : "dark-class"}>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="signup" element={<Signup />} />
-              <Route path="login" element={<Login />} />
-              <Route path="forgot-password" element={<ForgotPassword />} />
-            </Route>
-            <Route path="/profile" element={
+    <BrowserRouter>
+      <div className={theme === "light" ? "light-class" : "dark-class"}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+          </Route>
+          <Route
+            path="/profile"
+            element={
               <PetProvider>
-                <PetsLayout />
-              </PetProvider>}>
-              <Route index element={<Dashboard />} />
-              <Route path="petprofile" element={<PetProfile />} />
-              <Route path="petprofile/:petId" element={<PetUser />} />
-              <Route path="cage" element={<Cage />} />
-              <Route path="notifications" element={<Notifications />} />
-              <Route path="help" element={<Help />} />
-              <Route path="tank" element={<Tank />} />
-              <Route path="settings" element={<Settings />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+                <NotificationProvider>
+                  <PetsLayout />
+                </NotificationProvider>
+              </PetProvider>
+            }
+          >
+            <Route index element={<Dashboard />} />
+            <Route path="petprofile" element={<PetProfile />} />
+            <Route path="petprofile/:petId" element={<PetUser />} />
+            <Route path="cage" element={<Cage />} />
+            <Route path="notifications" element={<Notifications />} />
+            <Route path="help" element={<Help />} />
+            <Route path="tank" element={<Tank />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
