827a244ae82abf4b7f95d5fc885ce79ac5c7b315
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-12 02:26:03 +0800

    changes in indentation and added new eslit disable

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable arrow-parens */
 /* eslint-disable comma-dangle */
 /* eslint-disable quotes */
@@ -10,7 +12,8 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://petness-92c55-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://petness-92c55-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
 const region = "asia-southeast1";
@@ -22,12 +25,12 @@ const CACHE_TTL = 60 * 60 * 1000; // 1 hour
 // Helper function to get pet name from Firestore with caching
 async function getPetName(petId) {
   const cached = petNameCache[petId];
-  if (cached && (Date.now() - cached.timestamp < CACHE_TTL)) {
+  if (cached && Date.now() - cached.timestamp < CACHE_TTL) {
     return cached.name;
   }
   const petDoc = await admin.firestore().collection("pets").doc(petId).get();
   const petName = petDoc.data().name;
-  petNameCache[petId] = {name: petName, timestamp: Date.now()};
+  petNameCache[petId] = { name: petName, timestamp: Date.now() };
   return petName;
 }
 
@@ -36,31 +39,36 @@ function createNotificationMessage(title, body) {
   return {
     title: title,
     body: body,
-    timestamp: admin.firestore.FieldValue.serverTimestamp()
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
   };
 }
 
 // Function to send notification on new pet added
-exports.sendNotificationOnNewPet = functions.region(region).firestore
-    .document("pets/{petId}")
-    .onCreate(async (snapshot, context) => {
-      const newPet = snapshot.data();
-      const newPetMessage = createNotificationMessage(
-        `New Pet Added (${newPet.name})`,
-        `${newPet.name} has been created.`
-      );
+exports.sendNotificationOnNewPet = functions
+  .region(region)
+  .firestore.document("pets/{petId}")
+  .onCreate(async (snapshot, context) => {
+    const newPet = snapshot.data();
+    const newPetMessage = createNotificationMessage(
+      `New Pet Added (${newPet.name})`,
+      `${newPet.name} has been created.`
+    );
 
-      try {
-        await admin.firestore().collection("notifications").add({
-          title: newPetMessage.title,
-          body: newPetMessage.body,
-          petName: newPet.name,
-          timestamp: newPetMessage.timestamp,
-        });
-      } catch (error) {
-        console.error("Error sending new pet notification:", error.message, error.stack);
-      }
-    });
+    try {
+      await admin.firestore().collection("notifications").add({
+        title: newPetMessage.title,
+        body: newPetMessage.body,
+        petName: newPet.name,
+        timestamp: newPetMessage.timestamp,
+      });
+    } catch (error) {
+      console.error(
+        "Error sending new pet notification:",
+        error.message,
+        error.stack
+      );
+    }
+  });
 
 // Function to format pet record data into a readable string
 function formatPetRecord(record) {
@@ -72,64 +80,80 @@ function formatPetRecord(record) {
     `Mode: ${record.mode} Mode`,
     `Amount: ${record.amount}`,
     `Food Consumed: ${record.foodConsumed}`,
-    `Weight: ${record.weight}`
+    `Weight: ${record.weight}`,
   ];
-  return fields.filter(field => field.includes(': null') === false).join(', ');
+  return fields
+    .filter((field) => field.includes(": null") === false)
+    .join(", ");
 }
 
 // Function to send notification on new pet record added
-exports.sendNotificationOnNewPetRecord = functions.region(region).firestore
-    .document("pets/{petId}/records/{recordId}")
-    .onCreate(async (snapshot, context) => {
-      const newRecord = snapshot.data();
-      const {petId} = context.params;
-      const petName = await getPetName(petId);
+exports.sendNotificationOnNewPetRecord = functions
+  .region(region)
+  .firestore.document("pets/{petId}/records/{recordId}")
+  .onCreate(async (snapshot, context) => {
+    const newRecord = snapshot.data();
+    const { petId } = context.params;
+    const petName = await getPetName(petId);
 
-      const newRecordMessage = createNotificationMessage(
-        "New Pet Record Added for " + petName,
-        `There is a new record added for ${petName} with a specific dispense amount value of ${newRecord.amount}. 
+    const newRecordMessage = createNotificationMessage(
+      "New Pet Record Added for " + petName,
+      `There is a new record added for ${petName} with a specific dispense amount value of ${
+        newRecord.amount
+      }. 
         \nMore Details: \n${formatPetRecord(newRecord)}`
+    );
+
+    try {
+      await admin.firestore().collection("notifications").add({
+        title: newRecordMessage.title,
+        body: newRecordMessage.body,
+        petName: petName,
+        timestamp: newRecordMessage.timestamp,
+      });
+    } catch (error) {
+      console.error(
+        "Error sending new pet record notification:",
+        error.message,
+        error.stack
       );
+    }
+  });
 
-      try {
-        await admin.firestore().collection("notifications").add({
-          title: newRecordMessage.title,
-          body: newRecordMessage.body,
-          petName: petName,
-          timestamp: newRecordMessage.timestamp,
-        });
-      } catch (error) {
-        console.error("Error sending new pet record notification:", error.message, error.stack);
-      }
-    });
-
-  // Function to format feeding information data into a readable string
-  function formatFeedingInfo(feedingInfo) {
-    return `Scheduled Date: ${feedingInfo.scheduledDate}\nScheduled Time: ${feedingInfo.scheduledTime}\nAmount to Feed: ${feedingInfo.amountToFeed} 
+// Function to format feeding information data into a readable string
+function formatFeedingInfo(feedingInfo) {
+  return `Scheduled Date: ${feedingInfo.scheduledDate}\nScheduled Time: ${feedingInfo.scheduledTime}\nAmount to Feed: ${feedingInfo.amountToFeed} 
     \nRER: ${feedingInfo.RER} \nMER: ${feedingInfo.MER} \nFood Name: ${feedingInfo.foodSelectedName} & Calories Per Gram: ${feedingInfo.caloriesPerGram}`;
-  }
+}
 
 // Function to send notification on new feeding information added
-exports.sendNotificationOnLatestFeedingInfo = functions.region(region).firestore
-    .document("pets/{petId}/feedingInformations/{feedingInfoId}")
-    .onCreate(async (snapshot, context) => {
-      const newFeedingInfo = snapshot.data();
-      const {petId} = context.params;
-      const petName = await getPetName(petId);
+exports.sendNotificationOnLatestFeedingInfo = functions
+  .region(region)
+  .firestore.document("pets/{petId}/feedingInformations/{feedingInfoId}")
+  .onCreate(async (snapshot, context) => {
+    const newFeedingInfo = snapshot.data();
+    const { petId } = context.params;
+    const petName = await getPetName(petId);
 
-      const newFeedingInfoMessage = createNotificationMessage(
-        "New Feeding Schedule Added for " + petName,
-        `A new feeding schedule has been made for ${petName} with\n${formatFeedingInfo(newFeedingInfo)}.`
+    const newFeedingInfoMessage = createNotificationMessage(
+      "New Feeding Schedule Added for " + petName,
+      `A new feeding schedule has been made for ${petName} with\n${formatFeedingInfo(
+        newFeedingInfo
+      )}.`
+    );
+
+    try {
+      await admin.firestore().collection("notifications").add({
+        title: newFeedingInfoMessage.title,
+        body: newFeedingInfoMessage.body,
+        petName: petName,
+        timestamp: newFeedingInfoMessage.timestamp,
+      });
+    } catch (error) {
+      console.error(
+        "Error sending new feeding information notification:",
+        error.message,
+        error.stack
       );
-
-      try {
-        await admin.firestore().collection("notifications").add({
-          title: newFeedingInfoMessage.title,
-          body: newFeedingInfoMessage.body,
-          petName: petName,
-          timestamp: newFeedingInfoMessage.timestamp,
-        });
-      } catch (error) {
-        console.error("Error sending new feeding information notification:", error.message, error.stack);
-      }
-    });
+    }
+  });
