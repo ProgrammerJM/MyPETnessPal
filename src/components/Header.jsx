1a3ecd552f38d004b0fef783efa6799d67e77e78
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-15 13:53:55 +0800

    new: responsiveness feature and import of image in src/assets for optimized img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,50 @@
 import { Link as ScrollLink } from "react-scroll";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import violetBG from "../assets/images/bgViolet.jpg";
+import petnessLogoIcon from "../assets/images/petness-logo-icon.png";
 
 export default function Header() {
-  return (
-    <header className="bg-violetBG shadow-lg w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out">
-      <div className="flex items-center h-16">
-        <img
-          src="/images/petness-logo-icon.png"
-          alt="Petness"
-          className="h-16 w-auto pl-4 md:pl-10"
-        />
+  const location = useLocation();
 
-        <div className="flex items-center gap-6 md:gap-10 ml-auto">
+  // Check if we are on the Home route
+  const isHomeRoute = location.pathname === "/";
+
+  return (
+    <header
+      style={{ backgroundImage: `url(${violetBG})` }}
+      className="shadow-lg w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out"
+    >
+      <div className="flex items-center justify-between h-16 px-4 md:px-10">
+        <img src={petnessLogoIcon} alt="Petness" className="h-16 w-auto" />
+
+        <div className="flex items-center gap-6 md:gap-10">
           <Link
             to="/"
             className="text-lg font-medium px-4 py-2 text-light-darkViolet hover:text-indigo-900 hover:shadow-md transition duration-150 ease-in-out"
           >
             Home
           </Link>
-          <ScrollLink
-            to="about-section"
-            smooth={true}
-            duration={500}
-            className="cursor-pointer text-lg font-medium px-4 py-2 text-gray-600 hover:text-indigo-900 hover:shadow-md transition duration-150 ease-in-out"
-          >
-            About Us
-          </ScrollLink>
-          <ScrollLink
-            to="contact-section"
-            smooth={true}
-            duration={500}
-            className="cursor-pointer text-lg font-medium px-4 py-2 text-gray-600 hover:text-indigo-900 hover:shadow-md transition duration-150 ease-in-out"
-          >
-            Contact Us
-          </ScrollLink>
+          {isHomeRoute && (
+            <ScrollLink
+              to="about-section"
+              smooth={true}
+              duration={500}
+              className="md:text-center sm:text-center cursor-pointer text-lg font-medium px-4 py-2 text-gray-600 hover:text-indigo-900 hover:shadow-md transition duration-150 ease-in-out"
+            >
+              About Us
+            </ScrollLink>
+          )}
+          {isHomeRoute && (
+            <ScrollLink
+              to="contact-section"
+              smooth={true}
+              duration={500}
+              className="cursor-pointer text-lg font-medium px-4 py-2 text-gray-600 hover:text-indigo-900 hover:shadow-md transition duration-150 ease-in-out"
+            >
+              Contact Us
+            </ScrollLink>
+          )}
         </div>
       </div>
     </header>
