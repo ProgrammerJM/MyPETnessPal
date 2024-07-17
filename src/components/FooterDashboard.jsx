372abd882dff3e059f5fab370296e8d8dee05ed4
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-18 05:57:02 +0800

    remove type and fix responsiveness

--- src/components/FooterDashboard.jsx
+++ src/components/FooterDashboard.jsx
@@ -3,17 +3,17 @@ import petnessLogo from "../assets/images/petness-logo.png";
 export default function FooterDashboard() {
   return (
     <>
-      <footer className="footer bg-gray-700	p-6 flex text-center">
-        <div className="border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-3">
-          <div className="flex flex-row-reverse justify-center items-center">
+      <footer className="footer bg-gray-700 p-6 flex justify-center items-center text-center">
+        <div className="border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-3 w-full max-w-3xl">
+          <div className="flex justify-center items-center">
             <img
               src={petnessLogo}
-              className="h-12 mr-3 sm:h-20"
+              className="h-12 sm:h-20 mx-auto"
               alt="PETness"
             />
           </div>
-          <div className="text-sm text-center text-white mb-2">
-            © 2023-2324 PETNESS™. All Rights Reserved.
+          <div className="text-sm text-white mb-2">
+            © 2023-2024 PETNESS™. All Rights Reserved.
           </div>
         </div>
       </footer>
