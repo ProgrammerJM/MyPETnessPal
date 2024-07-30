eb376b075e82cd8ecc87eb12f7f65e53ddb614db
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-30 19:06:15 +0800

    chore: Update Firebase configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,6 @@ import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 import { getMessaging } from "firebase/messaging"; // Add the Firebase products that you need
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
